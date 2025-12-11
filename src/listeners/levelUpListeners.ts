bcd0014a81ec9b7cf99d7ef3e9b9c84703dc28b7
Lucas Rosenzweig <xeit0w@gmail.com>
2025-12-11 02:27:53 +0100

    Added todo and remove console.log on level up

--- src/listeners/levelUpListeners.ts
+++ src/listeners/levelUpListeners.ts
@@ -8,7 +8,8 @@ type User = Awaited<ReturnType<typeof prisma.user.findUnique>>;
 export function setupLevelUpListeners(client : Client) {
     client.on('levelUp', async (user : User, newLevel: number ) => {
         if(!user) return;
-        console.log(`Utilisateur ${user?.discordId} a atteint le niveau ${newLevel}`); // TODO : Message dans un channel spécifique ?
+        console.log(`Utilisateur ${user?.discordId} a atteint le niveau ${newLevel}`);
+        // TODO : Message dans un channel spécifique ?
 
         const guild = await client.guilds.fetch(user.guildId);
         const member = await guild.members.fetch(user.discordId);
@@ -30,15 +31,13 @@ export function setupLevelUpListeners(client : Client) {
 
         for (const roles of rolesToAdd) {
             try {
-                console.log(`Ajout du rôle ${roles.roleId} à l'utilisateur ${user.discordId}`); //TODO : Message dans un channel spécifique ?
+                console.log(`Ajout du rôle ${roles.roleId} à l'utilisateur ${user.discordId}`);
+                //TODO : Message dans un channel spécifique ?
                 await member.roles.add(roles.roleId);
             }
             catch (error) {
                 console.error(`Erreur lors de l'ajout du rôle ${roles.roleId} à l'utilisateur ${user.discordId}:`, error);
             }
         }
-
-
-
     });
 }
